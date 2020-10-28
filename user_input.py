8a688f2ae3631d70b5f96289413c488b77ab2961
Tara <traarj@gmail.com>
2020-10-28 13:27:28 +0100

    separated yes-no-question to its own function

--- user_input.py
+++ user_input.py
@@ -2,16 +2,7 @@ import re
 
 def user_input():
     # Check if the user would like to crawl the IMDb Top 1000
-    while True:
-        crawl_imdb = input("Would you like to crawl the IMDb Top 1000? y/n: ").lower()
-        if crawl_imdb == "y":
-            imdb = True
-            break
-        elif crawl_imdb == "n":
-            imdb = False
-            break
-        else:
-            print("You need to type either \"y\" for yes or \"n\" for no.")
+    imdb = yes_no_question("Would you like to crawl the IMDb Top 1000? y/n: ")
 
     # Check what URL the user would like to crawl
     if imdb == True:
@@ -39,19 +30,10 @@ def user_input():
             print("You have to type a number between 1 and 100.")
 
     # Check if the user would like to define a regex
-    while True:
-        define_regex = input("Would you like to define a regex to find sensitive data? y/n: ").lower()
-        if define_regex == "y":
-            user_defined_regex = True
-            break
-        elif define_regex == "n":
-            user_defined_regex = False
-            break
-        else:
-            print("You need to type either \"y\" for yes or \"n\" for no.")
+    define_regex = yes_no_question("Would you like to define a regex to find sensitive data? y/n: ")
     
     # Check what the user-defined regex is
-    if user_defined_regex == True:
+    if define_regex == True:
         while True:
             regex = input("What regex would you like to use? ")
             if regex[0] != "\"" or regex[len(regex) - 1] != "\"" or len(regex) < 3:
@@ -63,3 +45,16 @@ def user_input():
     
     #return variables
     return url, levels, regex
+
+def yes_no_question(question: str) -> bool:
+    while True:
+        answer = input(question).lower()
+        if answer == "y":
+            value = True
+            break
+        elif answer == "n":
+            value = False
+            break
+        else:
+            print("You need to type either \"y\" for yes or \"n\" for no.")
+    return value
